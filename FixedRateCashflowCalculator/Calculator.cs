ada3a865aec385edda0b25f24781dd8d4c77a13b
Syed Usama Amer <amers@carleton.edu>
2017-12-08 10:11:32 -0500

    Add cash flow calculator for single loan.

---
+++ FixedRateCashflowCalculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixedRateCashflowCalculator
+{
+    class Calculator
+    { 
+        public double CalculateInterest(double AmountLoaned, double Rate, double NumberOfMonths, double IthMonth) {
+        return IthMonth * (Rate / 1200);
+
+    }
+        public double CalculatePrincipal(double AmountLoaned, double Rate, double NumberOfMonths, double IthMonth) {
+         
+            return TotalMonthlyPayment(AmountLoaned, Rate, NumberOfMonths) - CalculateInterest(AmountLoaned, Rate, NumberOfMonths, IthMonth);
+        }
+        public double TotalMonthlyPayment(double AmountLoaned, double Rate, double NumberOfMonths)
+        {
+           double Base = (1 + (Rate / 1200));
+          double Numerator = (AmountLoaned) * (Rate / 1200);
+           return Numerator / (1 - Math.Pow(Base, (-NumberOfMonths)));
+        }
+  
+    }
+}
--- FixedRateCashflowCalculator/Program.cs
+++ FixedRateCashflowCalculator/Program.cs
@@ -10,6 +10,50 @@ namespace FixedRateCashflowCalculator
     {
         static void Main(string[] args)
         {
+
+            double LoanAmount = GetLoanAmount();
+            double NumberOfMonths = GetTerm();
+            double Rate = GetInterest();
+
+            Calculator calculator = new Calculator();
+            Console.Write("Month"+"\t"+"Interest"+"\tPrincipal"+"\t"+"Rem Balance"+ "\n");
+            Console.Write("\n");
+            ;
+            double RemainderIth = LoanAmount;
+            for (int i = 1; i < NumberOfMonths + 1; i++)
+            {
+                double DisplayInterest = calculator.CalculateInterest(LoanAmount, Rate, NumberOfMonths, RemainderIth);
+                double DisplayPrincipal = calculator.CalculatePrincipal(LoanAmount, Rate, NumberOfMonths, RemainderIth);
+                double DisplayRemaining = RemainderIth - DisplayPrincipal;
+                
+                Console.Write(i+"\t"+ Math.Round(DisplayInterest, 2) + "\t\t" + Math.Round(DisplayPrincipal, 2) + "\t\t" + Math.Round(DisplayRemaining, 2));
+                RemainderIth = DisplayRemaining;
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+
+
         }
+
+        static double GetLoanAmount()
+        {
+            Console.Write("Please enter your loan amount: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        static double GetTerm()
+        {
+            Console.Write("Please enter the number of months the loan will be paid out in: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        static double GetInterest()
+        {
+            Console.Write("Please enter the interest you want to be charged: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+
     }
 }
