78896213d93cc5f6f4eea30aa24252ac8c65c2b0
amers185 <31739308+amers185@users.noreply.github.com>
2017-12-17 19:15:39 -0800

    Update Calculator.cs
    Finished the application.

--- FixedRateCashflowCalculator/Calculator.cs
+++ FixedRateCashflowCalculator/Calculator.cs
@@ -1,27 +1,54 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FixedRateCashflowCalculator
+namespace CashflowCalculator
 {
     class Calculator
-    { 
-        public double CalculateInterest(double AmountLoaned, double Rate, double NumberOfMonths, double IthMonth) {
-        return IthMonth * (Rate / 1200);
-
-    }
-        public double CalculatePrincipal(double AmountLoaned, double Rate, double NumberOfMonths, double IthMonth) {
-         
-            return TotalMonthlyPayment(AmountLoaned, Rate, NumberOfMonths) - CalculateInterest(AmountLoaned, Rate, NumberOfMonths, IthMonth);
-        }
-        public double TotalMonthlyPayment(double AmountLoaned, double Rate, double NumberOfMonths)
+    {
+        private static decimal MonthlyPayment(decimal amount, int duration, decimal r) 
         {
-           double Base = (1 + (Rate / 1200));
-          double Numerator = (AmountLoaned) * (Rate / 1200);
-           return Numerator / (1 - Math.Pow(Base, (-NumberOfMonths)));
+            double numerator = Convert.ToDouble(amount * (r / 1200));
+            double denominator = (1 - Math.Pow(Convert.ToDouble(1 + r / 1200), -duration));
+            return Convert.ToDecimal(numerator / denominator);
         }
-  
+        private static decimal InterestCalc(decimal r, decimal remainBal)
+        {
+            decimal InterestPayment = remainBal * r / 1200;
+            return InterestPayment;
+        }
+        private static decimal PrincipalPay(decimal MonthlyPay, decimal InterestPayment)
+        {
+            decimal PrincipalPay = MonthlyPay - InterestPayment;
+            return PrincipalPay;
+        }
+
+        public static List<CashflowRow> CalculateCashflow(Loan loan)
+        {
+            List<CashflowRow> flowList = new List<CashflowRow>();
+            
+            decimal monthlyPay = MonthlyPayment(loan.Amount, loan.Duration, loan.Rate);
+            Console.WriteLine(monthlyPay);          
+            decimal RemainingBalance = loan.Amount;
+
+            for (int month = 1; month <= loan.Duration; month++)
+            {
+                CashflowRow flowRow = new CashflowRow();
+
+                flowRow.RemainingBalance = RemainingBalance;
+
+                flowRow.Month = month;
+                flowRow.InterestPayment = InterestCalc(loan.Rate, flowRow.RemainingBalance);
+                flowRow.PrincipalPayment = PrincipalPay(monthlyPay, flowRow.InterestPayment);
+                flowRow.RemainingBalance = flowRow.RemainingBalance - flowRow.PrincipalPayment;
+                flowList.Add(flowRow);
+
+                RemainingBalance = flowRow.RemainingBalance;
+            }
+            return flowList; 
+        }
+
     }
 }
